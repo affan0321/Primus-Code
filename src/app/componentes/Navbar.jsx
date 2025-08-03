062fae5d9da507904e951e0eabf17c1b48ebbf30
Affan Tahir <139456008+affan0321@users.noreply.github.com>
2025-08-04 00:42:21 +0500

    responsive landing page completed

--- src/app/componentes/Navbar.jsx
+++ src/app/componentes/Navbar.jsx
@@ -1,30 +1,117 @@
-import React from 'react';
+// import React from 'react';
+// import './Navbar.css';
+// export default function Navbar() {
+//   return (
+//     <div className="header">
+//         <div className="navbar">
+//           <div style={{width:"95%",margin:"0 auto",display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+//             <div className="logo">
+//                 <h5>PRIMUS <br /> CODE</h5>
+//         </div>
+//         <div className="nav-links">
+//             <ul>
+//                 <li>About Us</li>
+//                 <li>Services</li>
+//                 <li>Our Work</li>
+//                 <li>Blogs</li>
+//                 <button>Contact Us</button>
+//             </ul>
+//         </div>
+//     </div>
+//     </div>
+//     </div>
+    
+//   );
+// }
+
+
+
+// import React from 'react';
+// import './Navbar.css';
+// export default function Navbar() {
+//   return (
+//     <div className="header">
+//         <div className="navbar">
+//           <div style={{width:"95%",margin:"0 auto",display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+//             <div className="logo">
+//                 <h5>PRIMUS <br /> CODE</h5>
+//         </div>
+//         <div className="nav-links">
+//             <ul>
+//                 <li>About Us</li>
+//                 <li>Services</li>
+//                 <li>Our Work</li>
+//                 <li>Blogs</li>
+//                 <button>Contact Us</button>
+//             </ul>
+//         </div>
+//     </div>
+//     </div>
+//     </div>
+    
+//   );
+// }
+
+"use client"
+import React, { useState } from 'react';
 import './Navbar.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="header">
-        <div className="navbar">
-          <div style={{width:"95%",margin:"0 auto",display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-            <div className="logo">
-                <h5>PRIMUS <br /> CODE</h5>
-        </div>
-        <div className="nav-links">
+      <div className="navbar">
+        <div style={{ width: "95%", margin: "0 auto", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <div className="logo">
+            <h5>PRIMUS <br /> CODE</h5>
+          </div>
+
+          {/* Hamburger Icon */}
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </div>
+
+          {/* Navigation Links */}
+          <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
             <ul>
-                <li>About Us</li>
-                <li>Services</li>
-                <li>Our Work</li>
-                <li>Blogs</li>
-                <button>Contact Us</button>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Our Work</li>
+              <li>Blogs</li>
+              <button>Contact Us</button>
             </ul>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-    </div>
-    
   );
 }
 
-// "use client"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
 // import React, { useState } from 'react';
 // import './Navbar.css';
 
@@ -34,11 +121,9 @@ export default function Navbar() {
 //   return (
 //     <div className="header">
 //       <div className="navbar">
-//         <div className="navbar-container">
+//         <div className="navbar-inner">
 //           <div className="logo">
-//             <h5>
-//               PRIMUS <br /> CODE
-//             </h5>
+//             <h5>PRIMUS <br /> CODE</h5>
 //           </div>
 
 //           <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
@@ -61,4 +146,5 @@ export default function Navbar() {
 //     </div>
 //   );
 // }
+
 
